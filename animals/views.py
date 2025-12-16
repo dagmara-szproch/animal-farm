522afc2fdf8d7ff20a41fb0703b5df1e8371bd26
dagmara-szproch <dagmamara@gmail.com>
2025-12-16 14:30:03 +0000

    Add functionality to search bar

--- animals/views.py
+++ animals/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Animal, Category
 
 
@@ -15,10 +16,24 @@ class AnimalListView(ListView):
     context_object_name = 'animals'
 
     def get_queryset(self):
-        qs = Animal.objects.filter(is_active=True).select_related('category')
+        qs = Animal.objects.filter(is_active=True)
+
+        search = self.request.GET.get('q', '')
+        if search:
+            qs = qs.filter(
+                Q(name__icontains=search) |
+                Q(description__icontains=search) |
+                Q(category__name__icontains=search)
+            )
+
         category_slug = self.kwargs.get('slug')
         if category_slug:
             qs = qs.filter(category__slug=category_slug)
+
+        category = self.request.GET.get('category', '')
+        if category:
+            qs = qs.filter(category__name__iexact=category)
+
         return qs
 
 
